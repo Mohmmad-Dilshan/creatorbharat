162bdb539a92effe8ba357f28ed3ff1c0ee84fa0
Mohmmad Dilshan <dilsedilshan1@gmail.com>
2026-06-19 11:28:00 +0530

    Refactor deprecated Lenis import to renamed 'lenis' package

--- creator-bharat-v3/src/components/layout/PublicLayout.jsx
+++ creator-bharat-v3/src/components/layout/PublicLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, useNavigate } from 'react-router-dom';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 import { useApp } from '@/core/context';
 import Footer from '../home/Footer';
 import Navbar from './Navbar';
